827ca8e30d7ba059cd7b33795e953943c32c9bab
Sim KyouYong <SKY@Sim-ui-MacBook-Pro.local>
2017-07-07 23:13:53 +0900

    test

--- app/src/main/java/sky/kr/co/lockscreenbigenglish/MainActivity.java
+++ app/src/main/java/sky/kr/co/lockscreenbigenglish/MainActivity.java
@@ -2,7 +2,7 @@ package sky.kr.co.lockscreenbigenglish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
